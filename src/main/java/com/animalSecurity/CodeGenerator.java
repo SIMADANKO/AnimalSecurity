bc2c85f67d222270589a0347ced40ad7624461cd
Simadango <mayamalujia@gmail.com>
2024-12-07 01:13:01 +0800

    setup

---
+++ src/main/java/com/animalSecurity/CodeGenerator.java
@@ -0,0 +1,39 @@
+package com.animalSecurity;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.sql.Types;
+import java.util.Collections;
+
+public class CodeGenerator {
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/animalsec?useSSL=false&serverTimezone=UTC", "root", "123456")
+                .globalConfig(builder -> {
+                    builder.author("lu") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .outputDir(System.getProperty("user.dir") + "/src/main/java");// 指定输出目录
+                })
+                .dataSourceConfig(builder ->
+                        builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
+                            int typeCode = metaInfo.getJdbcType().TYPE_CODE;
+                            if (typeCode == Types.SMALLINT) {
+                                // 自定义类型转换
+                                return DbColumnType.INTEGER;
+                            }
+                            return typeRegistry.getColumnType(metaInfo);
+                        })
+                )
+                .packageConfig(builder ->
+                        builder.pathInfo(Collections.singletonMap(OutputFile.xml, System.getProperty("user.dir") + "/src/main/resources/mapper")) // 设置mapperXml生成路径
+                )
+                .strategyConfig(builder ->
+                        builder.addInclude("pets","orders","users","vendors") // 设置需要生成的表名
+                                 // 设置过滤表前缀
+                )
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
+}
---
+++ src/main/java/com/animalSecurity/lang/Result.java
@@ -0,0 +1,4 @@
+package com.animalSecurity.lang;
+
+public class Result {
+}
