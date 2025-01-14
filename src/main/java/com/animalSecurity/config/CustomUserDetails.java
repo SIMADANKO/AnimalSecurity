946b6b3ffa3cdfb0cb32941c3f1cd4603c2830da
Simadango <mayamalujia@gmail.com>
2025-01-14 16:49:08 +0800

    用户详细信息token中添加userId

---
+++ src/main/java/com/animalSecurity/config/CustomUserDetails.java
@@ -0,0 +1,64 @@
+package com.animalSecurity.config;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+    // 获取 userId
+    @Getter
+    private Integer userId;  // 用户ID
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    // 构造函数
+    public CustomUserDetails(Integer userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    // 实现 UserDetails 接口中的其他方法
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+}
