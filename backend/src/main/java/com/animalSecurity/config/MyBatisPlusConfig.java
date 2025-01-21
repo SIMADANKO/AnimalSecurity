4becb7d1f1f8ecd9d09222d13beed680a2c8c49e
Simadango <mayamalujia@gmail.com>
2025-01-22 07:31:00 +0800

    mybatis的分页配置

---
+++ backend/src/main/java/com/animalSecurity/config/MyBatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.animalSecurity.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 设置分页拦截器
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return interceptor;
+    }
+}
